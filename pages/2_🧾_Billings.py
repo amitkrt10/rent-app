81d1f0c225832598e2472b6cdeba5d66887e64d3
amitkrt-pe <amit.thakur@pharmeasy.in>
2022-11-01 20:33:44 +0530

    01-11-2

--- pages/2_🧾_Billings.py
+++ pages/2_🧾_Billings.py
@@ -100,7 +100,7 @@ if "login" in st.session_state.keys():
                 cellFontSize = 30
                 alignList = ['left','right']
                 ap.plot_table_with_title_total(title_text,subtitle_text,column_headers,cellText,colWidths,scaleY,headerFontSize,cellFontSize,alignList)
-                st.write(f"Your total rent due for the month of {selectMonth} is ₹ {viewList[8]}. Please pay the rent before 06/{viewList[0].strftime('%m/%Y')}. You can now view your bills and statements at https://kb-tenant.streamlitapp.com . Your username is {userId} and password is {pw}")
+                st.code(f"Your total rent due for the month of {selectMonth} is ₹ {viewList[8]}. Please pay the rent before 06/{viewList[0].strftime('%m/%Y')}. You can now view your bills and statements at https://kb-tenant.streamlitapp.com . Your username is {userId} and password is {pw}")
             except:
                 st.write("Bill Not Available")
 
