1e467827fdd199761d9eb60728db9bb057d41a94
amitkrt-pe <amit.thakur@pharmeasy.in>
2022-12-18 17:37:25 +0530

    contact

--- tenantView.py
+++ tenantView.py
@@ -104,8 +104,10 @@ if authentication_status:
     st.error("Please inform the owner before making any changes or repair to the property")
 
     st.markdown("<h3 style='text-align: center;text-shadow: 3px 2px gray;font-style: oblique;'>Owner Details</h3>", unsafe_allow_html=True)
-    st.markdown("<h6 style='font-style: oblique;'>R N Thakur - 70051 43261</h6>", unsafe_allow_html=True)
-    st.markdown("<h6 style='font-style: oblique;'>Amit Kumar - 89181 04083</h6>", unsafe_allow_html=True)
+    st.markdown("<h6 style='font-style: oblique;'>R N Thakur</h6>", unsafe_allow_html=True)
+    st.markdown("[70051 43261](callto:+917005143261)")
+    st.markdown("<h6 style='font-style: oblique;'>Amit Kumar</h6>", unsafe_allow_html=True)
+    st.markdown("[89181 04083](callto:+918918104083)")
 
 
 
