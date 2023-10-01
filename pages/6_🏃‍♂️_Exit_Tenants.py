a71eccdc63fa38540652d8fb0b3e6c4ff7eb8afb
amitkrt10 <amitkrt10@gmail.com>
2023-10-01 11:29:34 +0530

    exit calculation

--- pages/6_🏃‍♂️_Exit_Tenants.py
+++ pages/6_🏃‍♂️_Exit_Tenants.py
@@ -17,6 +17,9 @@ if "login" in st.session_state.keys():
     with st.spinner(text='Reading Data... Please Wait...!'):
         exitDueDict, exitTenantList = st.session_state["exitDueDict"], st.session_state["exitTenantList"]
         exitTenantDf, exitStatementDf = st.session_state["exitTenantDf"], st.session_state["exitStatementDf"]
+        tenantDf, activeFlatList = st.session_state["tenantDf"], st.session_state["activeFlatList"]
+        currentDueDf = st.session_state["currentDueDf"]
+        meterDf = st.session_state["meterDf"]
         st.balloons()
 
     st.markdown("<h2 style='text-align: center;text-shadow: 3px 2px blue;font-style: oblique;'>Exit Tenants</h2>", unsafe_allow_html=True)
@@ -101,6 +104,48 @@ if "login" in st.session_state.keys():
             cellFontSize = 40
             alignList = ['center','right','right','right']
             ap.plot_table_with_total(column_headers,cellText,colWidths,scaleY,headerFontSize,cellFontSize,alignList)
+
+    #Remove Tenant Calculation
+    with st.expander("Exit Calculation"):
+        selectedFlat = st.selectbox("Select Flat No.",activeFlatList,key="removetenant_calc")
+        tenantName = currentDueDf[currentDueDf.index==selectedFlat]['tenant_name'].values[0]
+        currentDue = currentDueDf[currentDueDf.index==selectedFlat]['dues'].values[0]
+        tenantSecurity = tenantDf[tenantDf.index==selectedFlat]['security_deposite'].values[0]
+        st.write(f"{tenantName} | Previous Due = ₹ {currentDue} | Secirity Deposite = ₹ {tenantSecurity}")
+        with st.form("Final Settlement",clear_on_submit=True):
+            rentAmt = st.text_input("Rent Amount",tenantDf[tenantDf.index==selectedFlat]["rent_amount"].values[0])
+            finalMeterReading = st.text_input("Final Meter Reading")
+            waterCharge = st.text_input("Water Charge",tenantDf[tenantDf.index==selectedFlat]["water_charge"].values[0])
+            garbageCharge = st.text_input("Garbage Charge",20)
+            otherCharges = st.text_input("Other Charge")
+            remark = st.text_input("Remark")
+            exitDate = st.date_input("Exit Date")
+            submitted = st.form_submit_button("Submit")
+            if submitted:
+                meterCharge = (int(finalMeterReading) - meterDf[meterDf.index==selectedFlat]["readings"].max()) * 10
+                finalDue = int(rentAmt) + int(meterCharge) + int(waterCharge) + int(garbageCharge) + int(otherCharges) + currentDue - tenantSecurity
+
+                cellText = []
+                cellText.append(["Rent",rentAmt])
+                cellText.append(["Electricity",meterCharge])
+                if waterCharge != 0:
+                    cellText.append(["Water",waterCharge])
+                if garbageCharge != 0:
+                    cellText.append(["Garbage",garbageCharge])
+                if otherCharges != 0:
+                    cellText.append([remark,otherCharges])
+                if currentDue != 0:
+                    cellText.append(["Previous Due",currentDue])
+                cellText.append(["Security Deposite (-)",tenantSecurity])
+                cellText.append(["Total",finalDue])
+
+                column_headers = [tenantName,selectedFlat]
+                colWidths = [2,1]
+                scaleY = 6
+                headerFontSize = 40
+                cellFontSize = 40
+                alignList = ['left','right']
+                ap.plot_table_with_total(column_headers,cellText,colWidths,scaleY,headerFontSize,cellFontSize,alignList)
 
 else:
     st.error("Please Login First...!")
